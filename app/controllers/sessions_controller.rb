0b5d5b825f8ebf4b657cb98f32947a043e395d22
Shafiqullah Qasemi <shafiqullah.qsm@gmail.com>
2023-09-09 17:21:22 +0300

    Add sessions create, destroy and fix current_user method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,4 +4,28 @@ class SessionsController < ApplicationController
 
   end 
 
+  def create 
+    user = User.find_by(username: params[:session][:username]) 
+    if user && user.authenticate(params[:session][:password]) 
+      session[:user_id] = user.id 
+      flash[:success] = "You have successfully logged in" 
+      redirect_to root_path 
+    else 
+      flash.now[:error] = "Incorrect login credentials, please try again" 
+      render :new, status: :unprocessable_entity 
+    end 
+  end 
+
+  def destroy 
+    session[:user_id] = nil 
+    flash[:success] = "You have successfully logged out" 
+    redirect_to login_path 
+  end 
+
+  private 
+
+  def set_user 
+    params(username: true, password: true)
+  end 
+
 end 
