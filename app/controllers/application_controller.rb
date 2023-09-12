9394472902b4c38497c1facc35da66d39056eaab
Shafiqullah Qasemi <shafiqullah.qsm@gmail.com>
2023-09-12 15:41:53 +0300

    Modify login page message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
 
   def require_user 
     if !logged_in? 
-      flash[:error] = "You must be logged in to perform that action" 
+      flash[:success] = "Welcome to MessageMe chat app!" 
       redirect_to login_path 
     end 
   end 
